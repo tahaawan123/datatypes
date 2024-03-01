2f25981a4bda82b58bb3f035fbbf4c9cc92867b6
MUHAMMAD TAHA <160233035+tahaawan123@users.noreply.github.com>
2024-03-02 01:17:51 +0500

    Add files via upload
    basic data type in strongly typed syntax and inference type

---
+++ data.js
@@ -0,0 +1,11 @@
+"use strict";
+//   strongly  Data syntax
+let firstName = "Muhammad Taha";
+let age = 23;
+let isMarried = false;
+console.log(firstName, age, isMarried);
+//  Inference Type
+let fName = "Muhammad Taha";
+let yourAge = 23;
+let married = false;
+console.log(fName, yourAge, married);
---
+++ data.ts
@@ -0,0 +1,11 @@
+
+//   strongly  Data syntax
+let firstName : string ="Muhammad Taha";
+let age  : number =23;
+let  isMarried : boolean =false;
+console.log(firstName, age, isMarried);
+//  Inference Type
+let fName="Muhammad Taha";
+let yourAge=23;
+let married=false;
+console.log(fName, yourAge, married);
